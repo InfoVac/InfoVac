9c1bce57174ed6e2e25cc2bb7433e3839f96b41a
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-11-26 20:18:06 -0300

    Delete TabelaDeVacinas directory

--- TabelaDeVacinas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Tabela_Disponibilidade
-from .models import Tabela_Funcionamento
-
-admin.site.register(Tabela_Disponibilidade)
-admin.site.register(Tabela_Funcionamento)
