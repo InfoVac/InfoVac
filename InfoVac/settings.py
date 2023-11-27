9c896401d60937b67b116ae61006a8c1137cd2a5
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-11-27 01:24:28 -0300

    Update settings.py

--- InfoVac/settings.py
+++ InfoVac/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'TabelaDeVacinas/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'TabelaDeVacinas/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
