ac82de8c6508a625118d6f54ff30b7cfe48f6013
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-11-27 00:29:44 -0300

    Update settings.py

--- InfoVac/settings.py
+++ InfoVac/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'InfoVac.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'ArquivosHTML')],
+        'DIRS': [os.path.join(BASE_DIR, 'TabelaDeVacinas/Templates/ArquivosHTML')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
