1348fba7c703264eb1994807e5b34d416ad349a0
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-11-27 17:51:55 -0300

    Update views.py

--- TabelaDeVacinas/views.py
+++ TabelaDeVacinas/views.py
@@ -5,23 +5,23 @@ from .models import Tabela_Funcionamento
 # Create your views here.
 
 def home(request):
-    return render(request, '/Templates/home.html')
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/home.html')
 def about(request):
-    return render(request, '/Templates/about.html')
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/about.html')
 def search(request):
     tabelas = Tabela_Disponibilidade.objects.all().order_by('nome_ubs', 'nome_vacina')
-    return render(request, '/Templates/search.html', {'tabelas': tabelas})
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/search.html', {'tabelas': tabelas})
 def buscar_disponibilidade(request):
     search_query = request.GET.get('search_query', '')
     tabelas = Tabela_Disponibilidade.objects.all()
     if search_query:
         from django.db.models import Q
         tabelas = tabelas.filter(Q(nome_ubs__icontains=search_query) | Q(nome_vacina__icontains=search_query))
-    return render(request, '/Templates/search.html', {'tabelas': tabelas})
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/search.html', {'tabelas': tabelas})
 def more_info(request):
     tabelas = Tabela_Funcionamento.objects.all().order_by('nome_ubs')
-    return render(request, '/Templates/more_info.html', {'tabelas': tabelas})
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/more_info.html', {'tabelas': tabelas})
 def buscar_horario(request):
     nome_ubs = request.GET.get('nome_ubs', '')
     tabelas = Tabela_Funcionamento.objects.filter(nome_ubs__icontains=nome_ubs)
-    return render(request, '/Templates/more_info.html', {'tabelas': tabelas})
+    return render(request, '/opt/render/project/src/TabelaDeVacinas/Templates/more_info.html', {'tabelas': tabelas})
