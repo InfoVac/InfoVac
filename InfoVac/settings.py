752cf52362b8e79c61e6488de7ea7fade5363cdb
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-11-27 00:03:35 -0300

    Update settings.py

--- InfoVac/settings.py
+++ InfoVac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9so!zt^ge0u^kumn$rr_58yj&e@jdcn+rhmrv$mb2gjo%)hqdu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infovac.onrender.com',]
 
 
 # Application definition
