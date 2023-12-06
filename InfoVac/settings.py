dc9dc252b7f6bb72c8a68bc07cd9e513a2d3992f
InfoVac <151162070+InfoVac@users.noreply.github.com>
2023-12-06 15:10:47 -0300

    Update settings.py

--- InfoVac/settings.py
+++ InfoVac/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'TabelaDeVacinas/static/'
+STATIC_URL = 'TabelaDeVacinas/static/TabelaDeVacinas/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
