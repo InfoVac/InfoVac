bf525f3242315b1ba077da8557d6c2a8a3a8c308
Julia Farias <150471918+juhfariasm@users.noreply.github.com>
2024-10-07 15:26:33 -0300

    Descrição das alterações

---
+++ TabelaDeVacinas/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('search', views.search, name='search'),
+    path('buscar_disponibilidade', views.buscar_disponibilidade, name='buscar_disponibilidade'),
+    path('more_info', views.more_info, name='more_info'),
+    path('buscar_horario', views.buscar_horario, name='buscar_horario'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ TabelaDeVacinas/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Tabela_Disponibilidade, Tabela_Funcionamento
+
+# Views
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def search(request):
+    tabelas = Tabela_Disponibilidade.objects.all().order_by('nome_ubs', 'nome_vacina')
+    return render(request, 'search.html', {'tabelas': tabelas})
+
+def buscar_disponibilidade(request):
+    search_query = request.GET.get('search_query', '')
+    tabelas = Tabela_Disponibilidade.objects.all()
+    if search_query:
+        from django.db.models import Q
+        tabelas = tabelas.filter(Q(nome_ubs__icontains=search_query) | Q(nome_vacina__icontains=search_query))
+    return render(request, 'search.html', {'tabelas': tabelas})
+
+def more_info(request):
+    tabelas = Tabela_Funcionamento.objects.all().order_by('nome_ubs')
+    return render(request, 'more_info.html', {'tabelas': tabelas})
+
+def buscar_horario(request):
+    nome_ubs = request.GET.get('nome_ubs', '')
+    tabelas = Tabela_Funcionamento.objects.filter(nome_ubs__icontains=nome_ubs)
+    return render(request, 'more_info.html', {'tabelas': tabelas})
