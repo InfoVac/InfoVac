0dc3d973b7e9d1bda9b7671c88c707b72ee48df0
InfoVac <151162070+InfoVac@users.noreply.github.com>
2024-10-11 12:32:16 -0300

    Update views.py

--- TabelaDeVacinas/views.py
+++ TabelaDeVacinas/views.py
@@ -16,8 +16,3 @@ def search(request):
 def buscar_disponibilidade(request):
     ubs_vacinas = UBSVacinas.objects.select_related('ubs', 'vacina').all()
     return render(request, 'lista_ubs_vacinas.html', {'ubs_vacinas': ubs_vacinas})
-
-def buscar_horario(request):
-    nome_ubs = request.GET.get('nome_ubs', '')
-    tabelas = Tabela_Funcionamento.objects.filter(nome_ubs__icontains=nome_ubs)
-    return render(request, 'more_info.html', {'tabelas': tabelas})
