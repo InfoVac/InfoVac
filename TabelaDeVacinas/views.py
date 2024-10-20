9ef41f00f1a1f50ab278fb9c990689c86c484711
InfoVac <151162070+InfoVac@users.noreply.github.com>
2024-10-20 15:00:28 -0300

    Update views.py

--- TabelaDeVacinas/views.py
+++ TabelaDeVacinas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import UBS, Vacinas, UBSVacinas, Tabela_Disponibilidade
+from .models import UBS, Vacina, UBSVacina
 
 # Views
 
@@ -10,14 +10,14 @@ def about(request):
     return render(request, 'about.html')
 
 def search(request):
-    tabelas = Tabela_Disponibilidade.objects.select_related('ubs', 'vacina').order_by('ubs__nome_ubs', 'vacina__nome_vacina')
+    tabelas = UBSVacina.objects.select_related('ubs', 'vacina').order_by('ubs__nome_ubs', 'vacina__nome_vacina')
     return render(request, 'search.html', {'tabelas': tabelas})
 
 def buscar_disponibilidade(request):
-    ubs_vacinas = UBSVacinas.objects.select_related('ubs', 'vacina').all()
+    ubs_vacinas = UBSVacina.objects.select_related('ubs', 'vacina').all()
     return render(request, 'lista_ubs_vacinas.html', {'ubs_vacinas': ubs_vacinas})
 
 def buscar_horario(request):
     nome_ubs = request.GET.get('nome_ubs', '')
-    tabelas = Tabela_Disponibilidade.objects.filter(ubs__nome_ubs__icontains=nome_ubs)
+    tabelas = UBSVacina.objects.filter(ubs__nome_ubs__icontains=nome_ubs)
     return render(request, 'more_info.html', {'tabelas': tabelas})
